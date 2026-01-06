c9c3c16559690415889917be213d4ae6a36bd98b
Shristi modi <shristimodi95@gmail.com>
2026-01-06 21:21:08 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+        <h1 className={"text-2xl underline"}> Welcome to my SaaS App</h1>
+        <Button>
+            Let&#39;s get started!
+        </Button>
+    </div>
   )
 }
 
